8980353d2874e28acfabf5ab57c6996261fef885
PirateGrunt <BFannin@RedwoodsGroup.com>
2015-01-17 09:05:26 -0500

    Short script to fetch Lahman data

---
+++ data/FetchLahmanData.R
@@ -0,0 +1,13 @@
+http://stackoverflow.com/questions/3053833/using-r-to-download-zipped-data-file-extract-and-import-data
+
+URL <- "http://seanlahman.com/files/database/lahman-csv_2014-02-14.zip"
+
+temp <- tempfile()
+download.file(URL,temp)
+
+# Have a look at the files to see what we're about to extract
+whatFiles <- unzip(temp, list = TRUE)
+View(whatFiles)
+
+unzip(temp, exdir = "./data/", junkpaths = TRUE)
+unlink(temp)
