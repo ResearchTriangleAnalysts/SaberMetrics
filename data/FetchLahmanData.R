57b3fd8f5fa8de9f380d313588b4a3486f74445b
PirateGrunt <BFannin@RedwoodsGroup.com>
2015-03-14 21:47:40 -0400

    Initial fun with datatable package

--- data/FetchLahmanData.R
+++ data/FetchLahmanData.R
@@ -1,9 +1,9 @@
-http://stackoverflow.com/questions/3053833/using-r-to-download-zipped-data-file-extract-and-import-data
+# http://stackoverflow.com/questions/3053833/using-r-to-download-zipped-data-file-extract-and-import-data
 
 URL <- "http://seanlahman.com/files/database/lahman-csv_2014-02-14.zip"
 
 temp <- tempfile()
-download.file(URL,temp)
+download.file(URL, temp)
 
 # Have a look at the files to see what we're about to extract
 whatFiles <- unzip(temp, list = TRUE)
